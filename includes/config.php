fda5949a1d504bb06f3d44062012b32fd4d35f6f
NusaibaNizam <saranusaibanizam@gmail.com>
2021-01-14 23:04:31 +0600

    4

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+	ob_start();
+	$timeZone=date_default_timezone_set("Asia/Dhaka");
+	$con=mysqli_connect("localhost", "root", "", "spotifyclone");
+	if(mysqli_connect_errno()){
+		echo "Faild to connect".mysqli_connect_errno();
+	}
+?>
